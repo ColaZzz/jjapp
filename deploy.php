432617d5525e35e9099d4dc1bea12d28293e299e
cola <ljlhyyc@qq.com>
2018-11-06 09:27:02 +0800

    1

--- deploy.php
+++ deploy.php
@@ -41,5 +41,23 @@ after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
 
-// before('deploy:symlink', 'artisan:migrate');
+before('deploy:symlink', 'artisan:migrate');
 
+// desc('Deploy your project');
+// task('deploy', [
+//     'deploy:info',
+//     'deploy:prepare',
+//     'deploy:lock',
+//     'deploy:release',
+//     'deploy:update_code',
+//     'deploy:shared',
+//     'deploy:vendors',
+//     'deploy:writable',
+//     'artisan:storage:link',
+//     'artisan:view:clear',
+//     // 'artisan:config:cache',
+//     'artisan:optimize',
+//     'deploy:symlink',
+//     'deploy:unlock',
+//     'cleanup',
+// ]);
