99a662ca1cf7c1f9c83187893bbf92ba8926c3ec
ColaZzz <ljlhyyc@qq.com>
2018-11-15 17:32:39 +0800

    3

--- config/wechat.php
+++ config/wechat.php
@@ -11,10 +11,10 @@ return [
         // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
         'response_type' => 'array',
     
-        'log' => [
-            'level' => 'debug',
-            'file' => __DIR__.'/wechat.log',
-        ],
+        // 'log' => [
+        //     'level' => 'debug',
+        //     'file' => __DIR__.'/wechat.log',
+        // ],
     ],
     
 ];
