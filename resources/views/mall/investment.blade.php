b0cd8990c366256e7c1501c8487ee4465c4f1bfa
ColaZzz <ljlhyyc@qq.com>
2018-11-30 17:37:44 +0800

    品牌部署

--- resources/views/mall/investment.blade.php
+++ resources/views/mall/investment.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>品牌招商</title>
 
     <!-- 最新版本的 Bootstrap 核心 CSS 文件 -->
